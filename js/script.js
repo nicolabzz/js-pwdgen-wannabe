78c52dc04ca1d004307a00553782b00da8bf82e6
nicolabzz <nbuzzigoli@gmail.com>
2022-10-19 14:02:42 +0200

    .First commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let name = prompt("Insert Name");
+let surname = prompt("Insert Surname");
+let favorite_color = prompt("Insert favoutite color");
+
+document.getElementById("passkey").innerHTML = "La passkey e': " + name + surname + favorite_color;
