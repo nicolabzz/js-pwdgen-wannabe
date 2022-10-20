e5083ccb5b4558f5952439435b80e4dd913974dc
nicolabzz <nbuzzigoli@gmail.com>
2022-10-20 09:56:50 +0200

    .like first commit but bertter

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ let name = prompt("Insert Name");
 let surname = prompt("Insert Surname");
 let favorite_color = prompt("Insert favoutite color");
 
+document.getElementById("name").innerHTML = "The insert name is:" + name;
+document.getElementById("surname").innerHTML = "The insert surname is:" + surname;
+document.getElementById("favorite-color").innerHTML = "The insert color is:" + favorite_color;
 document.getElementById("passkey").innerHTML = "La passkey e': " + name + surname + favorite_color;
